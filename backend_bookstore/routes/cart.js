d70b99923244723f5b53ee972e842046a03847e4
shubh <shubhamnarke04@gmail.com>
2025-07-01 23:14:52 +0530

    v1

--- backend_bookstore/routes/cart.js
+++ backend_bookstore/routes/cart.js
@@ -1,74 +1,74 @@
 const router = require("express").Router();
 const { model } = require("mongoose");
 const User = require("../models/user");
-const {authenticateToken} = require("./userAuth");
+const { authenticateToken } = require("./userAuth");
 
 // put book to cart 
 
-router.put("/add-to-cart",authenticateToken,async(req,res)=>{
+router.put("/add-to-cart", authenticateToken, async (req, res) => {
     try {
-        const {bookid , id}= req.headers();
-        const userData  = await User.findById(id);
+        const { bookid, id } = req.headers();
+        const userData = await User.findById(id);
         const isBookFavourited = userData.cart.includes(bookid);
-        if(isBookFavourited){
+        if (isBookFavourited) {
             return res.json({
-                status:"success",
-                message:"Book is allredy in cart",
+                status: "success",
+                message: "Book is allredy in cart",
             });
         }
-        await User.findByIdAndUpdate(id,{
-            $push:{cart:bookid},
+        await User.findByIdAndUpdate(id, {
+            $push: { cart: bookid },
         });
 
         return res.json({
-            stauts:"success",
-            message:"Book added to cart",
+            stauts: "success",
+            message: "Book added to cart",
         })
     } catch (error) {
         console.log(error);
-        return res.status(500).json({message:"An error occurred"});
-        
+        return res.status(500).json({ message: "An error occurred" });
+
     }
 });
 
 
 // remove to cart 
 
-router.put("/remove-to-cart/:bookid",authenticateToken ,async(req,res)=>{
+router.put("/remove-to-cart/:bookid", authenticateToken, async (req, res) => {
     try {
-        const {bookidid} = req.params;
-        const {id} = req.headers;
-        await User.findByIdAndUpdate(id,{
-            $pull:{cart:bookid},
+        const { bookidid } = req.params;
+        const { id } = req.headers;
+        await User.findByIdAndUpdate(id, {
+            $pull: { cart: bookid },
         });
 
         return res.json({
-            status:"success",
-            message:"Book removed from cart",
+            status: "success",
+            message: "Book removed from cart",
         });
-       
+
     } catch (error) {
         console.log(error)
-        return res.status(500).json({Message:"An error occured"})
+        return res.status(500).json({ Message: "An error occured" })
     }
 });
 
 
 // get cart of particular user 
 
-router.get("/get-user-cart",authenticateToken,async(req,res)=>{
+router.get("/get-user-cart", authenticateToken, async (req, res) => {
     try {
-       const  {id} = req.headers;
-       const userData = await User.findById(id).populate("cart");
-       const cart = userData.cart.reverse();
+        const { id } = req.headers;
+        const userData = await User.findById(id).populate("cart");
+        const cart = userData.cart.reverse();
 
-       return res.json({
-        status:"success",
-        data:cart,
-       });
+        return res.json({
+            status: "success",
+            data: cart,
+        });
     } catch (error) {
         console.log(error)
-        return res.status(500).json({message:"An errror occured"});
+        return res.status(500).json({ message: "An errror occured" });
     }
 });
 
